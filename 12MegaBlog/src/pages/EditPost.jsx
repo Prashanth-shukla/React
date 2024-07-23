74a86e674673912f00196a5c054261d57ddc3472
Prashanth-shukla <147225621+Prashanth-shukla@users.noreply.github.com>
2024-07-23 12:28:35 +0530

    Mega Project

---
+++ 12MegaBlog/src/pages/EditPost.jsx
@@ -0,0 +1,39 @@
+import React,{useEffect,useState} from 'react'
+import { Container } from '../components'
+import { PostForm } from '../components'
+import appwriteService from "../appwrite/config";
+import { useNavigate, useParams } from 'react-router-dom';
+
+function EditPost() {
+    const [post,setPosts] =useState(null)
+    const {slug}=useParams()
+    const navigate=useNavigate()
+    useEffect(() => {
+        if(slug){
+            appwriteService.getPost(slug).then((post)=>{
+                if(post){
+                    setPosts(post)
+               
+                }
+                })
+             } else{
+           
+                navigate('/')
+            }
+        }  ,[slug,navigate])
+      
+  return post?(
+    <div className='y-8'>
+        <Container>
+            <PostForm
+            post={post}/>
+        </Container>
+
+    </div>
+  ): null
+   
+  
+}
+
+
+export default EditPost
