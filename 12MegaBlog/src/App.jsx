7240b11c6e0b17c5f1d603d06ecf964c0fdb8e02
Prashanth-shukla <147225621+Prashanth-shukla@users.noreply.github.com>
2024-08-26 14:15:48 +0530

    Mega Blog using react

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -5,6 +5,7 @@ import authService from "./appwrite/auth"
 import {login, logout} from "./store/authSlice"
 import Header from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import { Editor } from '@tinymce/tinymce-react';
 
 import { Outlet } from 'react-router-dom'
 
@@ -30,10 +31,26 @@ function App() {
         <Header />
         <main>
         TODO:  <Outlet />
+        <Editor
+      apiKey='xl3mo9xwndeblug0voklqxdb1s0q5wo07hyj0opj1kzxrdob'
+      // init={{
+      //   plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+      //   toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+      //   tinycomments_mode: 'embedded',
+      //   tinycomments_author: 'Author name',
+      //   mergetags_list: [
+      //     { value: 'First.Name', title: 'First Name' },
+      //     { value: 'Email', title: 'Email' },
+      //   ],
+      //   ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+      // }}
+      //initialValue="Welcome to TinyMCE!"
+    />
         </main>
         <Footer />
       </div>
     </div>
+    
   ) : null
 }
 
--- 12MegaBlog/src/components/Logo.jsx
+++ 12MegaBlog/src/components/Logo.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Logo({width='100px'}) {
   return (
-    <div>Logo</div>
+   
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Virat_Kohli_in_PMO_New_Delhi.jpg/330px-Virat_Kohli_in_PMO_New_Delhi.jpg" alt="Nahi mila" />
+    
   )
 }
 
